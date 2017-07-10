f5024df4a1bb70bcc974e2a96cedb036f292aa11
takedajs <you@example.com>
2017-07-11 07:15:35 +0900

    現在見ているページが、登録したキーワードに一致するか確認する処理追加 余分な処理を削除

--- content_scripts/beastify.js
+++ content_scripts/beastify.js
@@ -1,29 +1,9 @@
-/*
-beastify():
-* removes every node in the document.body,
-* then inserts the chosen beast
-* then removes itself as a listener
-*/
 function beastify(request, sender, sendResponse) {
-  removeEverything();
-  insertText(request.beastURL);
-  browser.runtime.onMessage.removeListener(beastify);
+    console.log("aaaa")
+    browser.runtime.onMessage.removeListener(beastify);
 }
 
 /*
-Remove every node under document.body
-*/
-function removeEverything() {
-  while (document.body.firstChild) {
-    document.body.firstChild.remove();
-  }
-}
-
-function insertText(text) {
-    var beastImage = document.createElement("p");
-    var text = document.createTextNode(text);
-    beastImage.appendChild(text);
-    document.body.appendChild(beastImage);
-}
-
+ Assign beastify() as a listener for messages from the extension.
+ */
 browser.runtime.onMessage.addListener(beastify);
--- popup/choose_beast.js
+++ popup/choose_beast.js
@@ -1,16 +1,3 @@
-/*
- Given the name of a beast, get the URL to the corresponding image.
- */
-function beastNameToURL(beastName) {
-    switch (beastName) {
-        case "Frog":
-            return browser.extension.getURL("beasts/frog.jpg");
-        case "Snake":
-            return browser.extension.getURL("beasts/snake.jpg");
-        case "Turtle":
-            return browser.extension.getURL("beasts/turtle.jpg");
-    }
-}
 
 document.addEventListener("click", (e) => {
     if (e.target.classList.contains("save")) {
@@ -23,25 +10,38 @@ document.addEventListener("click", (e) => {
         }
 
         browser.storage.local.set({'value': array_keywords}, function() {
-            console.log("保存成功");
+            //console.log("保存成功");
         });
 
+        var storage_keywords = [];
         browser.storage.local.get('value', function(items) {
-            console.log(items.value);
+            storage_keywords = items.value;
         });
+
+        console.log(storage_keywords);
 
         browser.tabs.executeScript(null, {
             file: "/content_scripts/beastify.js"
         });
 
         var gettingActiveTab = browser.tabs.query({active: true, currentWindow: true});
-
         gettingActiveTab.then((tabs) => {
             console.log(tabs[0].url);
-        });
+            console.log(storage_keywords);
 
-        gettingActiveTab.then((tabs) => {
-            browser.tabs.sendMessage(tabs[0].id, {beastURL: test});
+            var tab_url = tabs[0].url;
+
+            // 現在見ているページが、登録したキーワードに一致するか確認
+            var isMatch = false;
+            for (var i = 0; i < storage_keywords.length; i++) {
+                var regexp = new RegExp(storage_keywords[i], 'i');
+                isMatch = regexp.test(tab_url);
+                if (isMatch) {
+                    break;
+                }
+            }
+
+            browser.tabs.sendMessage(tabs[0].id, {test: "aaa"});
         });
     } else if (e.target.classList.contains("clear")) {
         browser.tabs.reload();
